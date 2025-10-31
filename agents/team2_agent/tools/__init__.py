5871ec0ac8bcff5b29b19643d54d14425810c84d
yooshima <o.shimayu891@gmail.com>
2025-11-01 08:52:33 +0900

    Add hand contribution evaluation tool

--- agents/team2_agent/tools/__init__.py
+++ agents/team2_agent/tools/__init__.py
@@ -1,5 +1,6 @@
 from .pot_odds import pot_odds_tool
 from .hand_score import hand_score_tool
 from .hand_rank import hand_rank_tool
+from .hand_community import hand_community_tool
 
-__all__ = ["pot_odds_tool", "hand_score_tool", "hand_rank_tool"]
+__all__ = ["pot_odds_tool", "hand_score_tool", "hand_rank_tool", "hand_community_tool"]
